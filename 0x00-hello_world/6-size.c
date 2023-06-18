ccb51bd37111ae6c6a58130fe9e9ac4b12e82e55
azonto15 <jazzmtu4@gmail.com>
2023-06-18 05:39:26 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
-
 /**
- * main - prints the size of various types on
- * the computer it is compiled and run on..
- * Return: Always 0.
+ * main - 6-size.c
+ * description - Write a C program that prints the size of various types on
+ * the computer it is compiled and run on.
+ * Return: Always 0 (Success)
  */
 
 int main(void)
