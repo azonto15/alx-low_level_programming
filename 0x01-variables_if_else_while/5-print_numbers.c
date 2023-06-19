53a9bf677e77833accd58c503762bed24e4a0a21
azonto15 <jazzmtu4@gmail.com>
2023-06-19 13:47:15 -0700

    5-print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -6,18 +6,17 @@
  * main - main block
  * description:a program that prints all single digit numbers of base 10
  *starting from 0, followed by a new line.
- * followed by a new line.
- * return 0 on success
+ * Return: 0
  */
 int main(void)
 {
-        char c = '0';
+	char c = '0';
 
-        while (c <= '9')
-        {
-                putchar(c);
-                c++;
-        }
-        putchar('\n');
-        return (0);
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+		putchar('\n');
+		return (0);
 }
