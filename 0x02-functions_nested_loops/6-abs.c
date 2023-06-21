f84be6876b2f52867b67ad2fb151a4cbf24913b7
azonto15 <jazzmtu4@gmail.com>
2023-06-21 06:46:10 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * _abs - Computes the abslute value of an integer
+ * @c: The number to to be computed
+ * Return: Absoulte number of a vlaue or zero
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int abs_val;
+		
+		abs_val = c * -1;
+		return (abs_val);
+	}
+	return (c);
+}                                                                               
+                                  
