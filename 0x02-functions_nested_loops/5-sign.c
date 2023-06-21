1e5e1f1a455e0fa551bc20bf59654be5860abbbe
azonto15 <jazzmtu4@gmail.com>
2023-06-21 05:35:27 -0700

    5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include<stdio.h>
+/**
+* print_sign - check for lower case character
+* @c: The number to checked
+* Return: 1 for postive number or 0 for negative number and  naytheing else
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+			_putchar(45);
+			return (-1);
+	}
+	else
+	{
+		_putchar (48);
+		return (0);
+	}
+
+}
