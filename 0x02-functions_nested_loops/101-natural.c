639a9e642927fc5c160397f9c20978b05e0ec0b6
azonto15 <jazzmtu4@gmail.com>
2023-06-21 13:55:25 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*Main - list all the natural numbers below 10 that are multiples of 3 or 5,
+*Return: Always success
+*/
+int min(void)
+{
+
+int i, z = 0;
+
+while (i < 1024)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+{
+z += i;
+}
+i++;
+}
+printf("%d\n", z)
+return (0);
+}
