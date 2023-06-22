7c5523fe08f773b010721ad8db7c2034bd91889e
azonto15 <jazzmtu4@gmail.com>
2023-06-22 15:41:47 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,13 +5,13 @@
 *Return:Triangle
 */
 
-void print_triangle(int size);
+void print_triangle(int size)
 {
 int x, y, z;
 
 if (size <= 0)
 {
-_putchar("\n");
+_putchar('\n');
 }
 else
 {
@@ -19,14 +19,14 @@ for (x = 0; x < size; x++)
 {
 for (y = size - x; y > 1; y--)
 {
--putchar(32);
+_putchar(32);
 }
 for (z = 0; z <= x; z++)
 {
 _putchar(35);
 }
-_putchar("\n");
+_putchar('\n');
 
 }
-
+}
 }
