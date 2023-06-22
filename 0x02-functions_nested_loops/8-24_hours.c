86fc6a85302dbd71c55fb6654e557632ef793b00
azonto15 <jazzmtu4@gmail.com>
2023-06-21 17:06:51 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 /**
 *jack_bauer - function that prints every minute of the day of Jack Bauer,
 *starting from 00:00 to 23:59.
-Return: Always sucess.
+Return: Empty output.
 */
 
 void jack_bauer(void)
