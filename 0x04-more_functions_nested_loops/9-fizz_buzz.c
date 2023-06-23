3874f578df7580df8974be1499ed85d6c17040f7
azonto15 <jazzmtu4@gmail.com>
2023-06-23 08:58:33 -0700

    1-isdigit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,25 +12,25 @@ int x;
 
 for (x = 1; x <= 100; x++)
 {
-if ((x % 3 == 0) && (x % 5 == 0))
-{
-printf("FizzBuzz");
-}
-else if (x % 3 == 0)
+if (x % 3 == 0 && x % 5 != 0)
 {
 printf("Fizz");
 }
-else if (x % 5 == 0)
+else if (x % 5 == 0 && x % 3 != 0)
 {
 printf("Buzz");
 }
-else
+else if (x % 3 == 0 && x % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (x == 1)
 {
 printf("%d", x);
 }
-if (x != 100)
+else 
 {
-printf(" ");
+printf("%d", x);
 }
 }
 printf("\n");
