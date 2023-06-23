4bb7a44d9f455f0edbd26c3d93ea19bcedad44aa
azonto15 <jazzmtu4@gmail.com>
2023-06-23 09:41:51 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -24,13 +24,13 @@ else if (x % 3 == 0 && x % 5 == 0)
 {
 printf("FizzBuzz");
 }
-else if (x == 1)
+else
 {
 printf("%d", x);
 }
-else 
+if (x != 100)
 {
-printf("%d", x);
+printf(" ");
 }
 }
 printf("\n");
