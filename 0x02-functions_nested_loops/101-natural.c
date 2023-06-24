482cc537ab121fd3c4ee05fc0412672a1f2b27b0
azonto15 <jazzmtu4@gmail.com>
2023-06-24 12:38:17 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
 *Main - list all the natural numbers below 10 that are multiples of 3 or 5,
-*Return: The sum of multiles of 3 and 5
-*/                                                                              
+*Return: The sum of multiles of 3 and 5 below 1024
+*/
 
 int main(void)
 {
 int j, sum = 0;
 
-for (j = 0; j < 1024; ++j)
+for (j = 0; j < 1024; j++)
 {
 if ((j % 3) == 0 || (j % 5) == 0)
 sum += j;
