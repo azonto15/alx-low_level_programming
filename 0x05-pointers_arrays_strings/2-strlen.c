63e2581e7a70ab11fd5c4cc1e35e3a009b484ea9
azonto15 <jazzmtu4@gmail.com>
2023-06-26 11:19:36 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,6 +3,7 @@
 /**
 * _strlen - Returns the length of atsring
 * @*_strlen: The string which we will find the length
+* @str - a Variable member for string length
 * Return: The length of @str
 */
 int _strlen(char *str)
