27502f06388c913fab6c564b498a87e62bd1ac8b
azonto15 <jazzmtu4@gmail.com>
2023-07-03 12:05:54 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,12 +1,12 @@
  #include <unistd.h>                                                             
-  2                                                                                 
-  3 /**                                                                             
-  4 * _putchar - Writes the character c to stdout                                   
-  5 * @c:The character to print                                                     
-  6 * Return: on Success1.                                                          
-  7 * On error, -1 is returned, and eerno is set appropriately.                     
-  8 */                                                                              
-  9 int _putchar(char c)                                                            
- 10 {                                                                               
- 11 return (write(1, &c, 1));                                                       
- 12 }                        
+                                                                                  
+/**                                                                             
+* _putchar - Writes the character c to stdout                                   
+* @c:The character to print                                                     
+* Return: on Success1.                                                          
+* On error, -1 is returned, and eerno is set appropriately.                     
+*/                                                                              
+int _putchar(char c)                                                            
+{                                                                               
+return (write(1, &c, 1));                                                       
+  }                        
