126cbf109934b8bf1fb8629f12abc7129e15025a
azonto15 <jazzmtu4@gmail.com>
2023-09-04 13:14:11 -0700

    Updated file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,27 +3,24 @@
 #include <stdlib.h>
 
 char *create_buffer(char *file);
-void close_file(int fd);
+void close_file(int filedec);
 
 /**
- * create_buffer - This fuction allocates 1024 bytes for a buffer.
- * @file: This is a file variable name of the file buffer
- * is storing chars for.
- * Return: Returns a pointer variabe to the buffer that is created.
- */
+* create_buffer - This fuction allocates 1024 bytes for a buffer.
+* @file: This is a file variable name of the file buffer
+* is storing chars for.
+* Return: Returns a pointer variabe to the buffer that is created.
+*/
 char *create_buffer(char *file)
 {
 char *bafa;
-
 bafa = malloc(sizeof(char) * 1024);
-
 if (bafa == NULL)
 {
 dprintf(STDERR_FILENO,
 "Error: Can't write to %s\n", file);
 exit(99);
 }
-
 return (bafa);
 }
 
@@ -34,7 +31,6 @@ return (bafa);
 void close_file(int filedec)
 {
 int clos;
-
 clos = close(filedec);
 if (clos == -1)
 {
@@ -42,9 +38,8 @@ dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", filedec);
 exit(100);
 }
 }
-
 /**
- * main - The file that Copies file contents to another file.
+ * * main - The file that Copies file contents to another file.
  * @argc  arguments count passed to the program.
  * @argv: pointers collection(array)to the arguments.
  * Return: Returns 0 if it succeds.
@@ -63,12 +58,10 @@ if (argc != 3)
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-
 bafa = create_buffer(argv[2]);
 frm = open(argv[1], O_RDONLY);
 rd = read(frm, bafa, 1024);
 to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
 do {
 if (frm == -1 || rd == -1)
 {
@@ -77,7 +70,6 @@ dprintf(STDERR_FILENO,
 free(bafa);
 exit(98);
 }
-
 wrt = write(to, bafa, rd);
 if (to == -1 || wrt == -1)
 {
@@ -86,16 +78,12 @@ dprintf(STDERR_FILENO,
 free(bafa);
 exit(99);
 }
-
 rd = read(frm, bafa, 1024);
 to = open(argv[2], O_WRONLY | O_APPEND);
-
 } while (rd > 0);
-
 free(bafa);
 close_file(frm);
 close_file(to);
-
 return (0);
 }
 
